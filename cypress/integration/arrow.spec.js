e8d964f2dfd23f1584c7da5d9f9e4307dfc1f358
Luiz Paulo junior <luiz_paulojr@outlook.com>
2022-07-25 00:10:59 -0300

    Estudo basico sobre functions

--- cypress/integration/arrow.spec.js
+++ cypress/integration/arrow.spec.js
@@ -1,1 +1,29 @@
-it ()
+it ('por enquantoo nada',function(){
+
+})
+
+/*function soma (a, b){
+    return a + b;
+} 
+
+
+const soma = function(a, b){        Vai necessitar do return
+    return a + b;
+}
+*/
+
+
+//const soma = (a,b) => a + b       Nao precisa do return
+
+//0const soma = a => a + a
+
+let soma = () => 5 + 3
+
+console.log(soma(1,5));
+
+
+
+
+it('Arrow test',()=>{
+    console.log('Arrow', this)
+})
