235e3da948d89153cdf529fead188dbf830c7852
Luiz Paulo junior <luiz_paulojr@outlook.com>
2022-07-27 00:16:55 -0300

    Metodo assincrono: Promisse

--- cypress/integration/promise.spec.js
+++ cypress/integration/promise.spec.js
@@ -1,19 +1,32 @@
 it('sem testes', ()=>{ })
 
-    const getAlgo = callback =>{
+   /* const getAlgo = callback =>{
         setTimeout(()=>{
             
             callback(13)  //metodo callback para assincrono
         },1000)
     }
+    */
+    const getAlgo = () =>{
+        return new Promise((resolve, reject )=>{
+            
+            setTimeout(()=>{
+                resolve(13)
+            },1000)
+        })
+       
+    }
 
     const system = () =>{
-
+        
         console.log('init')
-        getAlgo(algo => {
-            console.log("algo é "+ algo)
+        getAlgo().then(algo =>{
+            console.log(`algo é ${algo}`)
             console.log('final')
         })
+           
+            
+        
     }
     
     system();
