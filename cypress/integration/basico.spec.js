47650dd4b3c0a04987c500273cbf102413283605
Luiz Paulo junior <luiz_paulojr@outlook.com>
2022-07-28 23:39:42 -0300

    Correção de it

--- cypress/integration/basico.spec.js
+++ cypress/integration/basico.spec.js
@@ -9,16 +9,21 @@ describe('Basico de Cypress',()=>{
         cy.title ()
             .should('be.equal','Campo de Treinamento') //estrutura padrao de assertivas (usando should ao inves de expect)
         
-        
-    cy.get('#buttonSimple')
-        .click()
-
-    cy.get('#formNome')
-        .type('joao')
-        .should('have.value','joao')
-        
+    
     
     })
+        it('Clicando no botao "clique me" e escrevendo no campo nome ',()=>{
+            cy.get('#buttonSimple')
+            .click()
+            .should('have.value','Obrigado!') //validando a interaçao apos o clique
+
+            cy.get('#formNome')
+            .type('joao')
+            .should('have.value','joao')
+            
+
+        })
+    
 })
 
 
