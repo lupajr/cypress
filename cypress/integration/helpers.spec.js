530fddf138f555c6d29e2ce21f7bc402748bcb28
Luiz Paulo junior <luiz_paulojr@outlook.com>
2022-08-10 22:51:33 -0300

    Usando wrap2

--- cypress/integration/helpers.spec.js
+++ cypress/integration/helpers.spec.js
@@ -11,8 +11,25 @@ describe('Helpers',()=>{
         cy.visit('https://www.wcaquino.me/cypress/componentes.html')
         cy.get('#formNome').then($el =>{    
             cy.wrap($el).type('joaozinho da silva')
-           // $el.val('aqui da sum')                      // printando usando Jquery
+           // $el.val('aqui da sim')                      // printando usando Jquery
         })
         
+
+        
     })  
+    describe('algo',()=>{
+        it('its',()=>{
+            let obj = {nome:'carlo',idade: 22}
+            cy.wrap(obj).should('have.property', 'nome')
+            cy.wrap(obj).its('nome').should('be.equal','carlo')
+
+            let obj2 = {nome:'carlo',idade: 22, endereco:{rua:'dos aes'}}
+
+            cy.wrap(obj2).its('endereco').should('have.property','rua')         //verificando se existe rua dentro de obj2
+            cy.wrap(obj2).its('endereco.rua').should('contain','dos')
+           })   
+    })
+
+    
+
 })
